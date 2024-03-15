d5e7f2aa5893551167cc1735389137260ddde7a9
Krzysiek <k.wiench@protonmail.com>
2024-03-15 10:35:52 +0100

    images upload done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import cors from "cors";
+import { v4 as uuidv4 } from "uuid";
 
 const upload = multer({ dest: "uploads/" });
 
@@ -11,26 +12,47 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.post("/api/photosUpload", upload.array("files"), (req, res) => {
-  console.log("Images uploaded");
-  console.log(req.files);
-  res.status(200).json({ success: true });
-});
+// const storage = multer.diskStorage(
+//   {
+//       destination: './uploads/',
+//       filename: function ( req, file, cb ) {
+//           //req.body is empty...
+//           //How could I get the new_file_name property sent from client here?
+//           cb( null, file.originalname+ '-' + Date.now()+".pdf");
+//       }
+//   }
+// );
 
-app.post("/api/tripDetails", (req, res) => {
-  const { name } = req.body;
-  const data = req.body;
-  console.log("boyd", name);
-  const filteredData = Object.entries(data).reduce((acc, [key, value]) => {
+const cleanData = (data) => {
+  return Object.entries(data).reduce((acc, [key, value]) => {
     if (value !== "") {
       acc[key] = value;
     }
     return acc;
   }, {});
+};
+
+app.post("/api/photosUpload", upload.array("files"), (req, res) => {
+  console.log("Data and images uploaded");
+
+  const data = req.body;
+  const filteredData = cleanData(data);
+  const id = uuidv4();
+
+  res.status(200).json({ success: true, tripDetails: filteredData, id });
+});
+
+app.post("/api/tripDetails", (req, res) => {
+  console.log("Uploaded data without images");
+  const data = req.body;
+
+  const filteredData = cleanData(data);
+  //generating uniqe id for new list item
+  const id = uuidv4();
 
   //TODO send data to database
 
-  res.status(200).json({ success: true, data: filteredData });
+  res.status(200).json({ success: true, tripDetails: filteredData, id });
 });
 
 app.listen(3000, () => {
